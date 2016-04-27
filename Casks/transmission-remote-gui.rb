9d5db0d029fff6a323b0ce33619318b7f50786b0
Cerv4ntez <WIRED79@gmx.at>
2016-04-27 09:20:51 +0200

    Update transmission-remote-gui.rb
    Devel Location seems to have changed and is now hosted on github.

--- Casks/transmission-remote-gui.rb
+++ Casks/transmission-remote-gui.rb
@@ -1,13 +1,12 @@
 cask 'transmission-remote-gui' do
-  version '5.0.1'
-  sha256 'b961aeb244b2519563837745f3475d21379e3da32bae2b3cbb20ca91d1a90d75'
+  version '5.3.0'
+  sha256 '45313d5fe3d503aa63624ceb8f04b08deeabbd51570480924502eecf9a795a83'
 
-  url "http://downloads.sourceforge.net/project/transgui/#{version}/transgui-#{version}.dmg"
+  url "https://github.com/leonsoft-kras/transmisson-remote-gui/releases/download//#{version}/Transmission.Remote.GUI.MacOS.app.zip"
   name 'Transmission Remote GUI'
-  homepage 'http://sourceforge.net/projects/transgui/'
+  homepage 'https://github.com/leonsoft-kras/transmisson-remote-gui/'
   license :oss
 
-  pkg 'transgui.pkg'
+  app 'Transmission.Remote.GUI.MacOS.app'
 
-  uninstall pkgutil: 'com.transgui.*'
 end
